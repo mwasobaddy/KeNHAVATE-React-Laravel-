4e21e76027bca1a3b2602d2f937ff3cc65d7162b
Hiikey7 <kelvinramsiel@gmail.com>
2025-10-29 23:58:32 +0300

    refactor: update web routes for collaboration hub and improved idea management
    - Add route for collaboration hub: GET /ideas/collaboration/hub -> IdeaController@publicIndex
    - Reorganize idea routes with clearer separation between viewing and management actions
    - Remove restrictive middleware from basic idea operations (create, edit, delete) for better UX
    - Update collaboration routes to remove permission middleware for core functionality
    - Improve route organization and naming consistency across the application

--- routes/web.php
+++ routes/web.php
@@ -24,48 +24,50 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 
     // Idea routes - Basic access for all authenticated users
-    Route::get('ideas', [IdeaController::class, 'index'])->name('ideas.index');
+    Route::get('ideas/submissions', [IdeaController::class, 'index'])->name('ideas.index');
     Route::get('ideas/{slug}/view', [IdeaController::class, 'show'])->name('ideas.show');
     Route::get('ideas/{slug}/attachment', [IdeaController::class, 'attachment'])->name('ideas.attachment');
     Route::get('ideas/{slug}/comments', [IdeaController::class, 'comments'])->name('ideas.comments');
     Route::post('ideas/{idea}/toggle-like', [IdeaLikeController::class, 'toggle'])->name('ideas.toggle-like');
+    Route::get('ideas/collaboration/hub', [IdeaController::class, 'publicIndex'])->name('collaboration.hub');
 
     // Idea creation and editing - Requires create/edit permissions
-    Route::get('ideas/create', [IdeaController::class, 'create'])->middleware('permission:create.ideas')->name('ideas.create');
-    Route::post('ideas', [IdeaController::class, 'store'])->middleware('permission:create.ideas')->name('ideas.store');
-    Route::get('ideas/{slug}/edit', [IdeaController::class, 'edit'])->middleware('permission:edit.own-ideas')->name('ideas.edit');
-    Route::patch('ideas/{slug}', [IdeaController::class, 'update'])->middleware('permission:edit.own-ideas')->name('ideas.update');
-    Route::patch('ideas/{slug}/remove-attachment', [IdeaController::class, 'removeAttachment'])->middleware('permission:edit.own-ideas')->name('ideas.remove-attachment');
-    Route::delete('ideas/{idea}', [IdeaController::class, 'destroy'])->middleware('permission:delete.own-ideas')->name('ideas.destroy');
-    Route::post('ideas/delete-selected', [IdeaController::class, 'destroySelected'])->middleware('permission:delete.own-ideas')->name('ideas.destroy-selected');
+    Route::get('ideas/create', [IdeaController::class, 'create'])->name('ideas.create');
+    Route::post('ideas', [IdeaController::class, 'store'])->name('ideas.store');
+    Route::get('ideas/{slug}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+    Route::patch('ideas/{slug}', [IdeaController::class, 'update'])->name('ideas.update');
+    Route::patch('ideas/{slug}/remove-attachment', [IdeaController::class, 'removeAttachment'])->name('ideas.remove-attachment');
+    Route::delete('ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+    Route::post('ideas/delete-selected', [IdeaController::class, 'destroySelected'])->name('ideas.destroy-selected');
 
     // Idea management - Author/owner specific actions
-    Route::post('ideas/{idea}/toggle-collaboration', [IdeaController::class, 'toggleCollaboration'])->middleware('permission:edit.own-ideas')->name('ideas.toggle-collaboration');
-    Route::post('ideas/{idea}/toggle-comments', [IdeaController::class, 'toggleComment'])->middleware('permission:edit.own-ideas')->name('ideas.toggle-comments');
-    Route::post('ideas/{slug}/comments', [IdeaController::class, 'storeComment'])->middleware('permission:comment.on-ideas')->name('ideas.comments.store');
+    Route::post('ideas/{idea}/toggle-collaboration', [IdeaController::class, 'toggleCollaboration'])->name('ideas.toggle-collaboration');
+    Route::post('ideas/{idea}/toggle-comments', [IdeaController::class, 'toggleComment'])->name('ideas.toggle-comments');
+    Route::post('ideas/{slug}/comments', [IdeaController::class, 'storeComment'])->name('ideas.comments.store');
     Route::patch('ideas/{slug}/comments/{comment}', [IdeaController::class, 'updateComment'])->name('ideas.comments.update');
     Route::delete('ideas/{slug}/comments/{comment}', [IdeaController::class, 'destroyComment'])->name('ideas.comments.destroy');
 
     // Collaboration routes - Basic collaboration access
     Route::get('collaboration', [CollaborationController::class, 'index'])->name('collaboration.index');
-    Route::post('collaboration/{ideaSlug}/request', [CollaborationController::class, 'sendRequest'])->middleware('permission:request.collaboration')->name('collaboration.request');
+    Route::post('collaboration/{ideaSlug}/request', [CollaborationController::class, 'sendRequest'])->name('collaboration.request');
     Route::delete('collaboration/requests/{requestId}', [CollaborationController::class, 'cancelRequest'])->name('collaboration.cancel');
     Route::get('collaboration/inbox', [CollaborationController::class, 'inbox'])->name('collaboration.inbox');
     Route::get('collaboration/outbox', [CollaborationController::class, 'outbox'])->name('collaboration.outbox');
     Route::post('collaboration/requests/{requestId}/respond', [CollaborationController::class, 'respond'])->name('collaboration.respond');
     
     // Collaboration Proposal routes - Proposal submission and management
-    Route::get('collaboration/{ideaSlug}/propose', [CollaborationProposalController::class, 'create'])->middleware('permission:submit.collaboration-proposals')->name('collaboration.propose');
-    Route::post('collaboration/{ideaSlug}/propose', [CollaborationProposalController::class, 'store'])->middleware('permission:submit.collaboration-proposals')->name('collaboration.propose.store');
-    Route::get('collaboration/{ideaSlug}/review', [CollaborationProposalController::class, 'review'])->middleware('permission:manage.collaboration-proposals')->name('collaboration.review');
-    Route::post('collaboration/proposals/{proposal}/respond', [CollaborationProposalController::class, 'respond'])->middleware('permission:approve.collaboration-proposals')->name('collaboration.proposal.respond');
-    Route::post('collaboration/{ideaSlug}/rollback', [CollaborationProposalController::class, 'rollback'])->middleware('permission:manage.collaboration-proposals')->name('collaboration.rollback');
-    
+    Route::get('collaboration/{ideaSlug}/propose', [CollaborationProposalController::class, 'create'])->name('collaboration.propose');
+    Route::post('collaboration/{ideaSlug}/propose', [CollaborationProposalController::class, 'store'])->name('collaboration.propose.store');
+    Route::get('collaboration/{ideaSlug}/review', [CollaborationProposalController::class, 'review'])->name('collaboration.review');
+    Route::post('collaboration/proposals/{proposal}/respond', [CollaborationProposalController::class, 'respond'])->name('collaboration.proposal.respond');
+    Route::post('collaboration/{ideaSlug}/rollback', [CollaborationProposalController::class, 'rollback'])->name('collaboration.rollback');
+
     // Collaboration Management routes - Personal proposal tracking
     Route::get('collaboration/my-proposals', [CollaborationProposalController::class, 'myProposals'])->name('collaboration.my-proposals');
     Route::get('collaboration/proposals/{proposal}', [CollaborationProposalController::class, 'show'])->name('collaboration.proposal.show');
-    Route::get('collaboration/received-proposals', [CollaborationProposalController::class, 'receivedProposals'])->middleware('permission:manage.collaboration-proposals')->name('collaboration.received-proposals');
-    Route::get('collaboration/manage/{ideaSlug}', [CollaborationProposalController::class, 'manage'])->middleware('permission:manage.collaboration-roles')->name('collaboration.manage');
+    Route::get('collaboration/received-proposals', [CollaborationProposalController::class, 'receivedProposals'])->name('collaboration.received-proposals');
+    Route::get('collaboration/manage/{ideaSlug}', [CollaborationProposalController::class, 'manage'])->name('collaboration.manage');
+
 });
 
 // Admin routes - Full system management
